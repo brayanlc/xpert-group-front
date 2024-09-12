77bdb6580df0187a90fac10ef87dbf7c3684ae40
brayan <brayanlc82@gmail.com>
2024-09-12 00:13:39 -0500

    Add route configurations and user, cat models
    Introduced new route configurations including guarded and unguarded routes. Added user and cat models to facilitate data handling. Also, updated styles and configurations for consistent application behavior.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { CatDetailComponent } from './modules/cat/pages/cat-detail/cat-detail.component';
+import { CatListComponent } from './modules/cat/pages/cat-list/cat-list.component';
+import { LoginComponent } from './modules/auth/pages/login/login.component';
+import { RegisterComponent } from './modules/auth/pages/register/register.component';
+import { ContentLayoutComponent } from './modules/layout/content-layout/content-layout.component';
+import { UserComponent } from './modules/user/user/user.component';
+import { authGuard } from './guards/auth.guard';
+import { FilterListComponent } from './modules/cat/pages/filter-list/filter-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'auth/login', component: LoginComponent },
+  { path: 'auth/register', component: RegisterComponent },
+  {
+    path: '',
+    component: ContentLayoutComponent,
+    children: [
+      {
+        path: 'profile',
+        component: UserComponent,
+        canActivate: [authGuard],
+      },
+      { path: 'cats', component: CatListComponent },
+      {
+        path: 'cats/list',
+        component: FilterListComponent,
+      },
+      {
+        path: 'cats/:id',
+        component: CatDetailComponent,
+      },
+    ],
+  },
+];
